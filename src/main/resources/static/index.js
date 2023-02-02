2a38cedff3b2154c231e992ac90660c5b8df7746
henriklarsen28 <henrik.hl@hotmail.com>
2023-02-02 09:59:27 +0100

    Kodegjennomgang

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,44 +1,84 @@
-window.onload = () => {
-    visBiler();
+window.onload = () =>{
+    visBiler()
 }
 let teller = 0;
+
 let biler = [
     {
         id: teller++,
-        merke:"Audi",
-        modell:"A6",
-        kjennetegn:"KH 235454"
+        merke: "Audi",
+        modell: "A6",
+        kjennetegn: "KJ234564"
     },
     {
-        id: teller++,
-        merke:"BMW",
-        modell:"330i",
-        kjennetegn:"KH 235453"
+        id:teller++,
+        merke: "BMW",
+        modell: "330i",
+        kjennetegn: "KJ464564"
     }
-];
+]
 
 function lagreBil(){
     let bil = {
-        id: teller++,
+        id:teller++,
         merke: document.getElementById("merke").value,
         modell: document.getElementById("modell").value,
         kjennetegn: document.getElementById("kjennetegn").value
     }
-    biler.push(bil);
-    console.log(biler);
-    visBiler();
-}
-function visBiler(){
-    let ut = "<table><tr><th>Merke</th><th>Modell</th><th>Kjennetegn</th></tr>";
-    for(let bil of biler){
-        ut += "<tr><td>" + bil.merke + "</td><td>" + bil.modell + "</td><td>" + bil.kjennetegn + "</td><td><button onclick='slettBil("+ bil.id +")'>Slett</button></td></tr>";
+    let errorCount = 0;
+
+    if(bil.merke == ""){
+        document.getElementById("merkeError").innerHTML = "Skriv inn et merke"
+        errorCount++
     }
-    ut += "</table>";
-    document.getElementById("biler").innerHTML = ut;
+    else{
+        document.getElementById("merkeError").innerHTML = ""
+    }
+    if(bil.modell == ""){
+        document.getElementById("modellError").innerHTML = "Skriv inn en modell"
+        errorCount++
+    }
+    else{
+        document.getElementById("modellError").innerHTML = ""
+    }
+    if (bil.kjennetegn == "" || bil.kjennetegn.length !== 9){
+        document.getElementById("kjennetegnError").innerHTML = "Skriv inn et kjennetegn"
+        errorCount++
+    }
+    else{
+        document.getElementById("kjennetegnError").innerHTML = ""
+    }
+
+    if(errorCount === 0){
+        biler.push(bil)
+        tomInput()
+        visBiler()
+    }
+
+
+
+
+}
+
+function tomInput(){
+    document.getElementById("merke").value = ""
+    document.getElementById("modell").value = ""
+    document.getElementById("kjennetegn").value = ""
+}
+
+function visBiler(){
+    let ut = "<table><tr><th>Merke</th><th>Modell</th><th>Kjennetegn</th></tr>"
+
+    for (let bil of biler){
+        ut += "<tr><td>"+ bil.merke +"</td><td>" + bil.modell + "</td><td>" + bil.kjennetegn + "</td><td><button onclick='slettBil(" + bil.id + ")'>Slett</button></td></tr>"
+    }
+
+    ut += "</table>"
+
+    document.getElementById("biler").innerHTML = ut
 }
 
 function slettBil(id){
-    console.log(biler[id])
-    biler = biler.filter(bil => bil.id !== id);
-    visBiler();
+    biler = biler.filter(bil => bil.id !== id)
+    visBiler()
 }
